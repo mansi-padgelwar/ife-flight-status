dd0a06554318fd4c30e146278ca954c14d17a715
mansi-padgelwar <mansi.padgelwar@airindia.com>
2023-11-27 13:45:26 +0530

    minor changes

--- data/flights.js
+++ data/flights.js
@@ -1,7 +1,7 @@
 export const flights = [
   {
     city: "New Delhi",
-    date: "Tue, 21 Nov",
+    date: "Tue, 21st Nov",
     details: [
       { title: "Scheduled Departure", subtitle: "4:00 am" },
       { title: "Terminal", subtitle: "3" },
@@ -10,7 +10,7 @@ export const flights = [
   },
   {
     city: "San Francisco",
-    date: "Tue, 21 Nov",
+    date: "Tue, 21st Nov",
     details: [
       { title: "Scheduled Departure", subtitle: "6:00 am" },
       { title: "Terminal", subtitle: "1" },
